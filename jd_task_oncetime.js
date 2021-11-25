ab6fce4f1ed768cd9ec0a124e97d374eab5be8fd
iaoongin <28687951+iaoongin@users.noreply.github.com>
2021-11-25 10:19:59 +0800

    feat. 执行一次的任务

---
+++ jd_task_oncetime.js
@@ -0,0 +1,38 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+var cron = require("node-cron");
+const moment = require("moment");
+const { stdout } = require("process");
+
+let jd_task_rawdata = fs.readFileSync("jd_task.json");
+let jd_task = JSON.parse(jd_task_rawdata);
+
+// console.log(jd_task)
+
+log(jd_task.name);
+
+for (let task of jd_task.list) {
+  log(`${task.name} \t ${task.time}`);
+
+  if (!task.running) {
+    log("配置跳过");
+    continue;
+  }
+
+  log(`manual: ${task.name}`);
+  execTask(task);
+}
+
+// log(cron.getTasks())
+
+// exec('node jd_bean_change.js')
+
+function log(message) {
+  let now = moment().format("YYYY-MM-DD HH:mm:ss");
+  console.log(`${now} ${message}`);
+}
+
+function execTask(task) {
+  // exec(`node ${task.job.target}`, (error, stdout) => console.log(stdout))
+  exec(`node ${jd_task.path}/${task.job.target}`);
+}
