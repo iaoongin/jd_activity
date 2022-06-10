076b157cddb1b0e317d867178998980c6ddea84f
iaoongin <28687951+iaoongin@users.noreply.github.com>
2022-06-10 15:24:56 +0800

    更新停止脚本

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
-ps aux | grep 'node jd_task.js' | awk '{print $2}' | xargs kill
-ps aux | grep 'node server/server.js' | awk '{print $2}' | xargs kill
-ps aux | grep 'jd_activity/web' | awk '{print $2}' | xargs kill
+ps aux | grep -v 'grep' | grep 'node jd_task.js' | awk '{print $2}' | xargs kill
+ps aux | grep -v 'grep' | grep 'node server/server.js' | awk '{print $2}' | xargs kill
+ps aux | grep -v 'grep' | grep 'jd_activity/web' | awk '{print $2}' | xargs kill
