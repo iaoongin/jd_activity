12e8c2ee8f9632410098e3f5541519ce19ade06b
iaoongin <28687951+iaoongin@users.noreply.github.com>
2022-09-27 01:14:40 +0800

    Update jd_dreamFactory.js

--- activity/jd_dreamFactory.js
+++ activity/jd_dreamFactory.js
@@ -1423,7 +1423,8 @@ function requireConfig() {
   return new Promise(async resolve => {
     console.log(`开始获取${$.name}配置文件\n`);
     //Node.js用户请在jdCookie.js处填写京东ck;
-    const shareCodes = $.isNode() ? require('./jdDreamFactoryShareCodes.js') : '';
+    // const shareCodes = $.isNode() ? require('./jdDreamFactoryShareCodes.js') : '';
+    const shareCodes = '';
     console.log(`共${cookiesArr.length}个京东账号\n`);
     $.shareCodesArr = [];
     if ($.isNode()) {
